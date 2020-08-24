446d33cf11375e4e40eca65e3471a309659fedd6
prarthna <prarthna.sahu@gmail.com>
2020-08-24 14:52:59 +0100

    First exercise001 commit

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,29 +1,39 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
-}
+  return word.charAt(0).toUpperCase() + word.slice(1);
+  }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
   // Add your code here!
+      return firstName.charAt(0) + '.' + lastName.charAt(0);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   // Add your code here!
+  let total = originalPrice + ((vatRate/100)*originalPrice);
+  return total;
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
   // Add your code here!
+  let saleprice = originalPrice - ((reduction/100)*originalPrice);
+  return saleprice;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
   // Add your code here!
+  let stringlength = str.length();
+  let middle = stringlength/2;
+  let middleCh = str.charAt(middle);
+  console.log(middleCh); 
 }
 
 function reverseWord(word) {
