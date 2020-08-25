3025332804338164d2a9d17023a412e6d9583f88
prarthna <prarthna.sahu@gmail.com>
2020-08-25 23:35:39 +0100

    exercise003 getsquare updated

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,6 +1,9 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
   // Your code here!
+   nums*nums;
+square = nums.map(getSquares);
+ return square;
 }
 
 function camelCaseWords(words) {
